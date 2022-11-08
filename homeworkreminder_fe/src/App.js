7ef877179e85d88448b6e853377b45b4cefd9a2f
Anh Bach <anh.bachtk@gmail.com>
2022-11-07 21:39:19 -0800

    Implement OAuth2 for Google

--- homeworkreminder_fe/src/App.js
+++ homeworkreminder_fe/src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+// import LoginButton from "./components/login";
+// import LogoutButton from "./components/logout";
+import {useEffect} from 'react';
+// import {gapi} from 'gapi-script';
+
+// const client_id = "499229867669-f080r1qa50rk020q00k838bllt5jqsmt.apps.googleusercontent.com";
 
 function App() {
+
+  function handleCallBackResponse(response){
+    console.log("Encoded JWT ID Token: " + response.credential);
+  }
+
+  useEffect(() => {
+     /* global google */ 
+    google.accounts.id.initialize({
+      client_id: "499229867669-f080r1qa50rk020q00k838bllt5jqsmt.apps.googleusercontent.com",
+      callback: handleCallBackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+
+    // function start() {
+    //   gapi.client.init({
+    //     clientId: client_id,
+    //     scope: ""
+    //   })
+    // };
+
+    // gapi.load('client:auth2', start);
+  }, []);
+
+  //var accessToken = gapi.auth.getToken().access_token;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className= "App">
+      <div id="signInDiv"></div>
+   </div>
   );
 }
 
