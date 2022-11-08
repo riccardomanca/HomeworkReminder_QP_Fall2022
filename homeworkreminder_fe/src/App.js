12b5b7a37902baf4613de686525ca6c4b8ee5fdc
Anh Bach <anh.bachtk@gmail.com>
2022-11-07 22:20:32 -0800

    OAuth 2 with Google
    sign in and sign out with Google Account

--- homeworkreminder_fe/src/App.js
+++ homeworkreminder_fe/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode'; // npm install jwt-decode 
 import './App.css';
-// import LoginButton from "./components/login";
-// import LogoutButton from "./components/logout";
-import {useEffect} from 'react';
-// import {gapi} from 'gapi-script';
 
-// const client_id = "499229867669-f080r1qa50rk020q00k838bllt5jqsmt.apps.googleusercontent.com";
+
 
 function App() {
+  const [user, setUser] = useState({});
 
   function handleCallBackResponse(response){
     console.log("Encoded JWT ID Token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut (event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+
   }
 
   useEffect(() => {
@@ -25,22 +34,20 @@ function App() {
       { theme: "outline", size: "large"}
     );
 
-    // function start() {
-    //   gapi.client.init({
-    //     clientId: client_id,
-    //     scope: ""
-    //   })
-    // };
-
-    // gapi.load('client:auth2', start);
   }, []);
-
-  //var accessToken = gapi.auth.getToken().access_token;
-
 
   return (
    <div className= "App">
       <div id="signInDiv"></div>
+      { Object.keys(user).length != 0 &&
+        <button onClick = {(e) => handleSignOut(e)}> Sign Out </button>
+      }
+      { user &&
+        <div>
+          <img src = {user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
    </div>
   );
 }
